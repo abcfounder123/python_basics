d6c94da5865293995a39e854aa3a93a80936462a
abcfounder123 <156555912+abcfounder123@users.noreply.github.com>
2024-09-30 20:32:12 +0700

    Update exercise.1

---
+++ exercises.py
@@ -0,0 +1,89 @@
+# exercises တွေပါ။
+# exercise.1
+
+b'apple'
+bytearray(b'apple')
+memoryview(b"apple")
+# <memory at 0x1005f2080>
+'1000'
+"1000"
+'''1000'''
+"""1000"""
+3 + 2j
+1j
+1000
+1000.0
+["apple", "banana", "orange"]
+[]
+list()
+()
+tuple()
+("Mg Mg", 20, 5.5)
+range(10)
+{"name": "Mg Mg", "age": 20, "height": 5.5}
+{
+    "name": "Mg Mg",
+    "age": 20,
+    "height": 5.5
+}
+{"apple", "banana", "orange"}
+{}
+set()
+frozenset()
+frozenset({'apple', 'orange', 'banana'})  # frozenset
+frozenset(['apple', 'orange', 'banana'])
+frozenset(('apple', 'orange', 'banana'))
+
+1000
+0
+int()
+True
+False
+bool()
+None
+
+{}
+{1, }
+
+(1)
+("apple")
+
+(1,)
+("apple",)
+1, 2, 3
+1,
+
+"1000"
+str(1000)
+
+1000
+int("1000")
+
+1000.0
+float(1000)
+float("1000")
+
+################################################
+
+# boolean value exercise
+
+0
+0.0
+
+1
+1000
+12.5
+0.1
+0.000001
+
+{}  # empty dict
+set()  # empty set
+[]  # empty list
+()  # empty tuple
+frozenset()  # empty frozenset
+""  # empty str
+''
+""""""
+''''''
+b""
+bytearray(b"")
