0abcc49618f30b4f56fcdcca35ebe6d84a6846f1
abcfounder123 <156555912+abcfounder123@users.noreply.github.com>
2025-07-13 02:07:32 +0700

    Create dcf.py
    notes

---
+++ dcf.py
@@ -0,0 +1,122 @@
+"""
+1. data type
+2. control flow
+3. function
+4. OOP
+5. module
+6. error handling
+7. FP
+8. Algorithm
+9. design patterns
+
+################################################
+
+1. data type
+
+unicode  1 (3)
+number   3
+list     8 (normal, unique, binary, dict, number)
+bool     1
+memoryview, None   2
+
+total 15, myan, Python
+
+################################################
+
+1. mutable, immutable
+2. unordered, ordered (indexing)
+3. explicit, implicit (empty -> False)
+   - empty data
+   - not empty
+4. homogeneous(same), heterogeneous
+
+################################################
+
+2. control flow
+   - t to b
+   - l to r
+   - inner parenthesis to outer one
+   - normal statements, conditional statements (if, elif, else)
+   - for, while, else  (iteration, control, opposite)
+
+################################################
+
+3. Function
+   - effect
+   - result  ( return )
+   - pure function
+   - unpure function
+
+1. effect only
+2. result only  ( pure function )
+3. effect & results
+
+################################################
+
+         parameter list ( tuple )
+       /
+def f():
+    pass
+
+################################################
+
+"""
+
+"""
+Ideas for decisions (if, elif, else)
+1. Greater number (if, else)
+2. Greater or equal number (if, elif, else)
+3. Largest of three numbers (if, elif, else)
+4. Ascending order of three numbers (nested if)
+5. Descending order of three numbers (nested if)
+6. Grade by marks ( Exercise )
+7. Time consumption & readability
+8. Dependent conditions & independent conditions
+9. Ticket price by age  ( Exercise )
+10. Combining conditions by using logical operator
+    - independent condition   -->   or
+    - dependent condition     -->   and
+    - negation, reverse       -->   not
+11. Checking leap year
+
+
+"""
+
+"""
+"for loop" 
+1. sum of first 10 numbers
+2. multiplication table of 7
+3. multiplication table
+4. reverse string
+5. counting vowels
+6. finding factorial ( n! )
+7. checking prime number
+8. Compound interest
+
+for loop (iteration)     --->   i or "proper name"
+for loop (repetition)    --->   "_" (underscore)
+
+while loop (control)     --->   condition check
+while loop (repetition)  --->   True  + break
+
+# else -> False
+# opposite between while & for  (because of opposite conditions)
+"""
+
+"""
+Parameter
+1. standard/ normal parameter    --->  def f(x, y, z)
+2. default parameter             --->   def area(r, pi=3.14):
+3. special parameter (/, *, *args, **kwargs)
+  - positional only parameter   --->  add(x, y, /, z, a=10)
+  - keword only parameter       --->  info(*, name, age, ph_no)
+  - variable-length positional parameter
+  - variable-length keyword parameter
+
+Arguments
+1. positional arguments       --->   f(2, 4, 6)
+2. keword arguments           --->   f(x=2, y=4, z=6), f(y=4, z=6, x=2)
+3. default arguments          --->  def area(r, pi=3.14):
+4. arbitrary positional arguments ( * )
+5. arbitrary keyword arguments ( ** )
+"""
