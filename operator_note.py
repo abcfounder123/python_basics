9215ab3144978a6f4f57ddc05b7c1a8e4bc3cf1b
abcfounder123 <156555912+abcfounder123@users.noreply.github.com>
2025-07-16 22:46:15 +0700

    Create operator_note.py
    add notes

---
+++ operator_note.py
@@ -0,0 +1,137 @@
+"""
+e, u, */, +-,  shift, and, or 2,  C, not, and, or,  t, assignment, warlus
+
+################################################
+
+1. Operand
+   >> left operand
+   >> right operand
+   
+     1    +    2
+    /     |     \
+  left    |     right
+       operator
+(addinional operation)  
+
+################################################
+
+2. Operator
+   >> binary operator  --->   left + right, 1 + 2   
+   
+   >> unary operator   --->   right 
+      1. unary plus    --->   + 1
+      2. unary minus   --->   - 1
+      3. logical NOT   --->   not True
+      4. bitwise NOT   --->   ~ 5
+
+################################################
+
+3. Precedence
+      
+################################################
+
+4. Associativity
+   >> left sided bind
+   >> right sided bind ( **, = ) (Assignment Operators 13)(Walrus)(lambda)
+
+################################################
+
+3. Precedence
+>> parenthesis        --->   ( )
+   
+>> Arithmetic operator (Perform mathematical operations)( e, u, * /, + - )(9)
+1. exponentiation     --->   **  
+2. unary +, -         --->   +1, -1, ~
+3. multiplication     --->   *
+   division   
+   division           --->   /
+   floor division     --->   //
+   modulus            --->   %
+4. addition           --->   +
+   substraction       --->   -
+
+>> Bitwise Operators (Operate on bits)(shift, and, or2, not)(6)
+5. Left shift         --->   <<
+   Right shift        --->   >>
+
+6. Bitwise AND        --->   &
+
+7. Bitwise XOR        --->   ^
+
+8. Bitwise OR         --->   |
+
+2. Bitwise NOT        --->   ~
+
+9. 
+Comparison Operators (Compare values)(6)
+   Equal to           --->   ==
+   Not equal to       --->   !=
+   Greater than       --->   >
+   Less than          --->   <
+   Greater than or equal to   --->   >=
+   Less than or equal to      --->   <=
+
+Identity Operators (Compare memory locations)
+   Identical objects          --->   is
+   Not identical objects      --->   is not
+
+Membership Operators (Test for membership in sequences)
+   Present in the sequence    --->   in
+   Not present in the sequence --->   not in
+
+>> Logical Operators (Combine conditional statements)
+10.Logical NOT        --->   not
+11.Logical AND        --->   and
+12.Logical OR         --->   or
+14.Assignment Operators (Assign values to variables) (13)
+    Simple assignment --->   =
+    Add and assign    --->   +=
+
+################################################
+################################################
+
+Python operator precedence 
+e, u, */, +-, 
+shift, and, or 2, 
+Cim, not, and, or, 
+t, assignment, warlus
+
+1. Exponentiation ** 
+2. Unary Operators (+, -, ~) (Unary positive, negative, and logical NOT, bitwise NOT) 
+3. Multiplication, Division, Modulus, Floor Division (*, /, %, //) 
+4. Addition and Subtraction (+, -) 
+
+5. Bitwise Shift Operators  (<<, >>) 
+6. Bitwise AND (&)    all
+7. Bitwise XOR (^)    only any
+8. Bitwise OR (|)     any
+
+9. Comparison Operators (==, !=, >, <, >=, <=)   (value)
+   Identity Operators (is, is not)               (memory address)
+   Membership Operators (in, not in) 
+10. Logical NOT (not)   opposite
+11. Logical AND (and)   all
+12. Logical OR (or)     any
+
+13. conditional operator, ternary operator   (choice value)
+14. Assignment Operators (13) 
+15. Special operator
+    Walrus operator (assignment expression)(:= ( နောက်ဆုံးမှာရှိ )
+      
+    3. Matrix multiplication ( @ ) ( same as multiplication )
+
+9 + 6 + 10 + 16 = 41 + 1 + 2  = 44
+
+################################################
+
+right sided bind (16) (**, =) 
+1. **
+2. Assignment Operators 13
+3. Walrus Operator
+
+
+4. lambda expression
+    
+################################################
+
+"""
