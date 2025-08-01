2b7a77dc71e653ed00aef85bc02e25b8237e5cc3
abcfounder123 <156555912+abcfounder123@users.noreply.github.com>
2025-08-01 23:18:03 +0700

    Create programming.py

---
+++ programming.py
@@ -0,0 +1,38 @@
+
+"""
+
+Target
+    - air speed, map, moisture, sun, cloud
+
+Programming
+- google api free(air speed, map, moisture, sun, cloud)
+- website 
+  - user interface ( html, css, bootstrap )
+  - program ( Python )
+  - data base ( SQL )
+
+################################################
+
+Three phases of a program
+1. Input
+2. Processing
+3. Output
+
+################################################
+
+Imperative Paradigm
+1. Procedural Programming
+2. Object Oriented Programming
+
+################################################
+
+Procedural Programming
+1. Sequences(top to buttom, left to right, parenthesis first)
+2. Selection(if, else)
+3. Iteration(for, while)
+4. Functions
+
+################################################
+
+"""
+
