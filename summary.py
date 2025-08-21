9246cec3d100a445ab14b0b90a705b67c3963d4f
abcfounder123 <156555912+abcfounder123@users.noreply.github.com>
2025-08-21 17:18:45 +0700

    Create summary.py

---
+++ summary.py
@@ -0,0 +1,39 @@
+"""
+
+
+1. Sequence 
+
+2. Usages of data types 
+
+3. Names of data types
+
+4. Symbols of data types
+ 
+5. Checking data types 
+
+6. Boolean values of empty and something 
+
+7. Mutable, Immutable, Ordered, Unordered 
+
+7. Indexing 
+
+8. Slicing 
+
+10. Operation, Operator, Operand (43)
+
+11. Unary, Binary, Ternary operator 
+
+12. Precedence (15)
+
+13. Associativity (2)
+
+14. Built-in Operators ( 7 )
+
+15. Arithmetic operators in programming (35)
+
+16. Normal statement, Conditional statement (if, elif, else)
+
+17. Program flow, Control flow, Program quality
+
+
+"""
